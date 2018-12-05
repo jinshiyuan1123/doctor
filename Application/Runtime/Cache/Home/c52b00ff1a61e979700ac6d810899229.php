884ae74df2e9a8997cb198e21b70d96a6035708a
soubei <2824030364@qq.com>
2018-12-05 17:53:45 +0800

    .

--- Application/Runtime/Cache/Home/c52b00ff1a61e979700ac6d810899229.php
+++ Application/Runtime/Cache/Home/c52b00ff1a61e979700ac6d810899229.php
@@ -1,9 +1,9 @@
-
+<?php if (!defined('THINK_PATH')) exit();?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <link rel="shortcut icon" href="__PUBLIC_HOME__/favicon.ico" />
+    <link rel="shortcut icon" href="/Public/home/favicon.ico" />
     <meta name="baidu-site-verification" content="Rk9zXLfIEQ" />
     <title>医生说｜糖尿病也能治愈！它的最佳治愈期你错过了吗？ - 择医网（挂号网）</title>
     <meta charset="utf-8">
@@ -12,12 +12,12 @@
     <meta name="keywords" content="医生说｜糖尿病也能治愈！它的最佳治愈期你错过了吗？,医生说,糖尿病前期,高血糖,大血管病,糖尿病" />
 <link rel="canonical" href="https://bbs.guahao.com/topic/yWOls32914951727316993"/>
 <!-- External CSS -->
-    <link rel="stylesheet" href="__PUBLIC_HOME__/css/topic/health.base.min.css?v=1540800883543" type="text/css"/>
+    <link rel="stylesheet" href="/Public/home/css/topic/health.base.min.css?v=1540800883543" type="text/css"/>
 
 
 
-    <link rel="stylesheet" href="__PUBLIC_HOME__/css/topic/common/common.css?_=1540800883543" type="text/css"/>
-        <link rel="stylesheet" href="__PUBLIC_HOME__/css/topic/index.css?_=1540800883543" type="text/css"/>
+    <link rel="stylesheet" href="/Public/home/css/topic/common/common.css?_=1540800883543" type="text/css"/>
+        <link rel="stylesheet" href="/Public/home/css/topic/index.css?_=1540800883543" type="text/css"/>
 </head>
 <body>
 
@@ -25,9 +25,9 @@
 
 <div class="g-header">
     <div class="g-header-box">
-        <a href="{:U('home/index/index')}" >
+        <a href="<?php echo U('home/index/index');?>" >
       
-          <img class="g-logo "   src="__PUBLIC_HOME__/images/logolist.jpg" alt="择医网" class="wy-logo">
+          <img class="g-logo "   src="/Public/home/images/logolist.jpg" alt="择医网" class="wy-logo">
         </a>
         <div class="g-login">
              <!--    您好！请 <span id="J_Login" class="login">登录</span> | <span class="register" id="J_Register">注册</span> -->
@@ -40,10 +40,10 @@
             <div class="g-wenzhang">
                 <div class="g-title">
                         <span class="g-icon">原创</span>
-                    <span class="span-title">医生说｜{$reslist.inspection_name}</span>
+                    <span class="span-title">医生说｜<?php echo ($reslist["inspection_name"]); ?></span>
                 </div>
                 <div class="g-time">
-                    <span class="span-time">{$reslist.create_time|date='Y-m-d H:i',###}</span>
+                    <span class="span-time"><?php echo (date('Y-m-d H:i',$reslist["create_time"])); ?></span>
                     <span class="span-yyt">【健康号】</span>
                     <span class="span-zz">
                             医生说
@@ -54,7 +54,7 @@
                         <span class="J_filter">拒绝糖尿病，从干预糖尿病前期开始！</span>
                     </div>
                     <div id="J_TopicContent" class="g-zw">
-                    {$reslist.unit}
+                    <?php echo ($reslist["unit"]); ?>
                     </div>
             </div>
             <div class="g-share">
@@ -90,37 +90,37 @@
                     <div class="tj-list">
                             <div class="tj-content">
                             <a class="img-con-div" href="" id="J_relatedImgCon">
-                                <img class="tj-img" src="__PUBLIC_HOME__/images/default-cover.png" style="width:260px;height:153px;">
+                                <img class="tj-img" src="/Public/home/images/default-cover.png" style="width:260px;height:153px;">
                             </a>
                             <a href="/topic/vIDim26461660889841665" target="_blank" class="tj-title" title="结果发现喝醋者能使餐后血糖下降">结果发现喝醋者能使餐后血糖下降</a>
                         </div>
                             <div class="tj-content">
                             <a class="img-con-div" href="" id="J_relatedImgCon">
-                                <img class="tj-img" src="__PUBLIC_HOME__/images/default-cover.png" style="width:260px;height:153px;">
+                                <img class="tj-img" src="/Public/home/images/default-cover.png" style="width:260px;height:153px;">
                             </a>
                             <a href="/topic/bnCiA188326" target="_blank" class="tj-title" title="高血糖饮食禁忌有什么？">高血糖饮食禁忌有什么？</a>
                         </div>
                             <div class="tj-content tj-last">
                             <a class="img-con-div" href="" id="J_relatedImgCon">
-                                <img class="tj-img" src="__PUBLIC_HOME__/images/default-cover.png" style="width:260px;height:153px;">
+                                <img class="tj-img" src="/Public/home/images/default-cover.png" style="width:260px;height:153px;">
                             </a>
                             <a href="/topic/UVDzI189880" target="_blank" class="tj-title" title="七招帮你搞定高血糖">七招帮你搞定高血糖</a>
                         </div>
                             <div class="tj-content">
                             <a class="img-con-div" href="" id="J_relatedImgCon">
-                                <img class="tj-img" src="__PUBLIC_HOME__/images/default-cover.png" style="width:260px;height:153px;" >
+                                <img class="tj-img" src="/Public/home/images/default-cover.png" style="width:260px;height:153px;" >
                             </a>
                             <a href="/topic/puJRQ177553" target="_blank" class="tj-title" title="尿糖和血糖，谁才是导致糖尿病的罪魁祸首？">尿糖和血糖，谁才是导致糖尿病的罪魁祸首？</a>
                         </div>
                             <div class="tj-content">
                             <a class="img-con-div" href="" id="J_relatedImgCon">
-                                <img class="tj-img" src="__PUBLIC_HOME__/images/default-cover.png" style="width:260px;height:153px;">
+                                <img class="tj-img" src="/Public/home/images/default-cover.png" style="width:260px;height:153px;">
                             </a>
                             <a href="/topic/qEuya111085" target="_blank" class="tj-title" title="高血糖饮食清单">高血糖饮食清单</a>
                         </div>
                             <div class="tj-content tj-last">
                             <a class="img-con-div" href="" id="J_relatedImgCon">
-                                <img class="tj-img" src="__PUBLIC_HOME__/images/default-cover.png" style="width:260px;height:153px;">
+                                <img class="tj-img" src="/Public/home/images/default-cover.png" style="width:260px;height:153px;">
                             </a>
                             <a href="/topic/ycHcr201816" target="_blank" class="tj-title" title="引起空腹高血糖的原因，不能只知其一不知其二">引起空腹高血糖的原因，不能只知其一不知其二</a>
                         </div>
@@ -262,7 +262,61 @@
             </div>
         </div>
 <div class="g-bottom">
-   <include file="Public:bottom"/>
+     <div id="gf">
+            <div class="g-container">
+                <div class="g-footer g-clear">
+                    <a class="logo g-left" href="/" onmousedown="return _smartlog(this,'BTM')"></a>
+                    <div class="links g-left">
+                        <dl class="one g-clear">
+                            <dt>关于择医网</dt>
+                            <dd><a href="<?php echo U('home/index/about');?>" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">关于我们</a></dd>
+                            <dd><a href="#" onmousedown="return _smartlog(this,'BTM')">友情链接</a></dd>
+                            <dd><a href="#" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">加入择医网</a></dd>
+                            <dd><a href="<?php echo U('home/index/statement');?>" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">版权声明</a></dd>
+                            <dd><a href="#" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">联系我们</a></dd>
+                            <dd class="last"><a href="#" rel="nofollow" target="_blank" onmousedown="return _smartlog(this,'BTM')">新浪微博</a></dd>
+                            <dd><a href="<?php echo U('home/index/navlist');?>" onmousedown="return _smartlog(this,'BTM')">网站地图</a></dd>
+                            <dd><a href="#" onmousedown="return _smartlog(this,'BTM')">知识库</a></dd>
+                        </dl>
+                        <dl class="two">
+                            <dd class="partner"><a href="#" onmousedown="return _smartlog(this,'BTM')">医院合作</a></dd>
+                            <dd class="help"><a href="#" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">客服中心</a></dd>
+                        </dl>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="code g-left">
+                        <a class="nihe" href="http://www.nihe.org.cn" rel="nofollow" target="_blank">中国健康教育中心<br/>卫生部新闻宣传中心</a>
+                        <a class="cha" href="http://www.cha.org.cn/" rel="nofollow" target="_blank">中国医院协会<br/>门急诊管理专业委员会</a>
+                        <a class="fudanmed" href="http://www.fudanmed.com/manage/index.aspx" rel="nofollow" target="_blank">上海复旦医院管理公司</a>
+                        <a class="cfnews" href="http://www.cfnews.org.cn" rel="nofollow" target="_blank">中国家庭报<br/>卫计委人口文化发展中心</a>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="internation g-right">
+                        <a href="http://icih.h.gov.cn/" rel="nofollow" target="_blank"><i></i>
+                                <h3>国际互联网医疗大会</h3>
+                                <p>International Conference</p>
+                                <p>on Internet Healthcare</p>
+                            </a>
+                    </div>
+                </div>
+                <p class="copyright">
+                    版权所有：杭州广发科技有限公司&nbsp;&nbsp;&nbsp;
+                        <a href="" rel="nofollow">浙B2-20130007</a>&nbsp;&nbsp;
+                        <a href="" rel="nofollow">浙卫(03)网审[2014]015号</a>&nbsp;&nbsp;
+                        <a href="" rel="nofollow">(浙)-经营性-2014-0022</a>&nbsp;&nbsp;
+                        <a class="alliance" href="" target="_blank" rel="nofollow"></a>
+                        <a class="alliance attestation" href="" target="_blank" rel="nofollow"></a>
+                        <a class="record" href="" rel="nofollow" target="_blank"></a>
+
+                </p>
+                <div style="width:300px;margin:0 auto; padding:20px 0;">
+                    <a target="_blank" href="" rel="nofollow" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="/Public/home/images/ghs.png" style="float:left;" />
+                        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">苏ICP备16006821号-2</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
 </div>
 </div>
 
