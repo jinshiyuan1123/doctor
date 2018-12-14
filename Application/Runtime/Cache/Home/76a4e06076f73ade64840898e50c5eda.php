b05f1c54ce0b52bbeb984b92f0969f5ee11e8247
soubei <2824030364@qq.com>
2018-12-14 14:05:55 +0800

    .

--- Application/Runtime/Cache/Home/76a4e06076f73ade64840898e50c5eda.php
+++ Application/Runtime/Cache/Home/76a4e06076f73ade64840898e50c5eda.php
@@ -1,4 +1,4 @@
-
+<?php if (!defined('THINK_PATH')) exit();?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -18,10 +18,10 @@
 <meta property="wb:webmaster" content="3b0138a4c935e0f6" />
 <meta property="qc:admins" content="341606771467510176375" />
 <meta name="shenma-site-verification" content="4c64bf7d4d5c6796e6aeac71a190af95_1465296479">
-	<link rel="shortcut icon" href="__PUBLIC_HOME__/favicon.ico" />
-	 <link rel="stylesheet" href="__PUBLIC_HOME__/css/portal.base.min.css?v=1535436384237752" type="text/css" />
-      <script src="__PUBLIC_HIS__/vendor/jquery/jquery.min.js"></script>
-    <script src="__PUBLIC_HIS__/vendor/layer/layer.js"></script>
+	<link rel="shortcut icon" href="/Public/home/favicon.ico" />
+	 <link rel="stylesheet" href="/Public/home/css/portal.base.min.css?v=1535436384237752" type="text/css" />
+      <script src="/Public/his/vendor/jquery/jquery.min.js"></script>
+    <script src="/Public/his/vendor/layer/layer.js"></script>
 
 
 
@@ -39,11 +39,53 @@
 <div id="g-wrapper" class="g-wrapper g-page-1200"> 
 	<div id="gh">
 
-<include file="Public:headlist"/> 
+<div class="gh-nav">
+    <div class="container g-clear">
+        <div class="g-left">
+            <?php if(!session('home_user_info')): ?><span class="text">您好！ 请</span>
+                <span class="login"></span>
+                <a href="<?php echo U('home/index/login');?>" rel="nofollow" onmousedown="return _smartlog(this,'TOP')" monitor="public_head,publick_head,login">登录</a>
+                <span class="line">|</span>
+                <a target="_blank" href="<?php echo U('home/index/register');?>" rel="nofollow" onmousedown="return _smartlog(this,'TOP')" monitor="public_head,publick_head,signin">注册</a>
+                <a href="<?php echo U('home/index/doctorlogin');?>" rel="nofollow" onmousedown="return _smartlog(this,'TOP')" class="doctor-login" monitor="public_head,publick_head,doctoruser"><i style="right:6px"><img src="/Public/home/doctor-login.png"></i>我是医生</a>
+            <?php else: ?>
+
+                     <span class="text">您好
+                         <?php echo session('home_user_info.mobile');?>
+                          </span> 
+                         
+                     <a href=" <?php if($docmobile): echo U('home/index/doctorhome'); else: echo U('home/index/orderlist'); endif; ?>" rel="nofollow" onmousedown="return _smartlog(this,'TOP')" monitor="public_head,publick_head,mycenter">个人中心</a>
+                       
+                    <span class="line">|</span>
+                    <a href="https://www.guahao.com/my/orderlist" rel="nofollow" onmousedown="return _smartlog(this,'TOP')" monitor="public_head,publick_head,order">预约单</a> 
+                    <span class="line">|</span>
+                    <a href="https://www.guahao.com/my/favorite/list" rel="nofollow" onmousedown="return _smartlog(this,'TOP')" monitor="public_head,publick_head,myfollow">我的关注</a> 
+                    <span class="line">|</span> 
+                    <a href="<?php echo U('/home/index/logout');?>" rel="nofollow" onmousedown="return _smartlog(this,'TOP')" monitor="public_head,publick_head,logout">退出</a><?php endif; ?>
+        </div>
+        
+        
+          <div class="g-right">
+                <a href="" rel="nofollow" class="dl-guide J_SeoGuide" onmousedown="return _smartlog(this,'TOP')" monitor="public_head,publick_head,appdload">
+                    收藏本站    
+                    <span>
+                        <img src="https://static.guahao.cn/front/portal-pc-static/img/index-appQRcode.png?v=20170505" alt="">
+                    </span>
+                </a>
+                <span class="line">|</span>
+
+
+          
+            <a href="tencent://message/?Site=baidu.com&uin=1569602446&Menu=yes" rel="nofollow" onmousedown="return _smartlog(this,'TOP')" id="helpCenter" target="_blank" monitor="public_head,publick_head,cscenter">客服中心</a>
+            
+
+        </div>
+      </div> 
+  </div> 
 			    <div class="gh-main">
 			     <div class="container g-clear">
 			    	<div class="gh-logo ">
-			    		<img class="logolist "  style="background-size: cover;width: 190px;height: 89px;display: inline-block;" src="__PUBLIC_HOME__/images/logolist.jpg" alt="择医网" class="wy-logo">
+			    		<img class="logolist "  style="background-size: cover;width: 190px;height: 89px;display: inline-block;" src="/Public/home/images/logolist.jpg" alt="择医网" class="wy-logo">
 			    	</div>
 
 	
@@ -96,11 +138,29 @@
 
 	<div class="container g-clear  container-small  ">
 		<div class="home-menu-ctn">
-			  <include file="Public:headtop" />
+			  <ul class="g-clear main-menu-nav">
+<li class="hot">
+    <a class="own-tab" target="_blank" href="<?php echo U('home/index/hospital');?>" monitor="public_menu,home_menu,ask">在线问诊</a>
+</li>
+<li><a target="_blank" href="<?php echo U('home/index/fastorder');?>" rel="nofollow" onmousedown="return _smartlog(this,'MNU')" monitor="public_menu,home_menu,fastpoder">医生论坛</a></li>
+<li><a target="_blank" target="_blank" href="<?php echo U('home/index/ask');?>" rel="nofollow" onmousedown="return _smartlog(this,'MNU')" monitor="public_menu,home_menu,preciseorder">免费咨询</a></li>
+<li class="authority"><a class="own-tab" target="_blank" target="_blank" href="https://baike.h.gov.cn/" rel="nofollow">医药直通车</a></li>
+<li data-subnav="sub-nav-welife"><a target="_blank" rel="nofollow" onmousedown="return _smartlog(this,'MNU')">
+医疗器械</a>
+</li>
+
+
+<li><a target="_blank" href="#" rel="nofollow" onmousedown="return _smartlog(this,'MNU')">健康E务室</a></li>
+  <li><a target="_blank" href="/Public/music" rel="nofollow" onmousedown="return _smartlog(this,'MNU')">优选音乐</a></li>
+
+   <li><a target="_blank" href="<?php echo U('home/index/video');?>" rel="nofollow" onmousedown="return _smartlog(this,'MNU')">微视频</a></li>
+
+
+</ul>
 
 
 			<div class="main-category-nav J_DiseaseCategory">
-				<a href="{:U('home/index/index')}">
+				<a href="<?php echo U('home/index/index');?>">
 				<h3><i></i>首页</h3></a>
 
 
@@ -424,7 +484,61 @@
 
 		</div>
 		
-		<include file="Public:bottom"/>
+		  <div id="gf">
+            <div class="g-container">
+                <div class="g-footer g-clear">
+                    <a class="logo g-left" href="/" onmousedown="return _smartlog(this,'BTM')"></a>
+                    <div class="links g-left">
+                        <dl class="one g-clear">
+                            <dt>关于择医网</dt>
+                            <dd><a href="<?php echo U('home/index/about');?>" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">关于我们</a></dd>
+                            <dd><a href="#" onmousedown="return _smartlog(this,'BTM')">友情链接</a></dd>
+                            <dd><a href="#" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">加入择医网</a></dd>
+                            <dd><a href="<?php echo U('home/index/statement');?>" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">版权声明</a></dd>
+                            <dd><a href="#" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">联系我们</a></dd>
+                            <dd class="last"><a href="#" rel="nofollow" target="_blank" onmousedown="return _smartlog(this,'BTM')">新浪微博</a></dd>
+                            <dd><a href="<?php echo U('home/index/navlist');?>" onmousedown="return _smartlog(this,'BTM')">网站地图</a></dd>
+                            <dd><a href="#" onmousedown="return _smartlog(this,'BTM')">知识库</a></dd>
+                        </dl>
+                        <dl class="two">
+                            <dd class="partner"><a href="#" onmousedown="return _smartlog(this,'BTM')">医院合作</a></dd>
+                            <dd class="help"><a href="#" rel="nofollow" onmousedown="return _smartlog(this,'BTM')">客服中心</a></dd>
+                        </dl>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="code g-left">
+                        <a class="nihe" href="http://www.nihe.org.cn" rel="nofollow" target="_blank">政府网<br/>卫生部新闻宣传中心</a>
+                        <a class="cha" href="http://www.cha.org.cn/" rel="nofollow" target="_blank">中国医院协会<br/>门急诊管理专业委员会</a>
+                        <a class="fudanmed" href="http://www.fudanmed.com/manage/index.aspx" rel="nofollow" target="_blank">中国阳网在线</a>
+                        <a class="cfnews" href="http://www.cfnews.org.cn" rel="nofollow" target="_blank">中国家庭报<br/>卫计委人口文化发展中心</a>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="internation g-right">
+                        <a href="http://icih.h.gov.cn/" rel="nofollow" target="_blank"><i></i>
+                                <h3>国际互联网医疗大会</h3>
+                                <p>International Conference</p>
+                                <p>on Internet Healthcare</p>
+                            </a>
+                    </div>
+                </div>
+                <!-- <p class="copyright">
+                    版权所有：&nbsp;&nbsp;&nbsp;
+                        <a href="" rel="nofollow">浙B2-20130007</a>&nbsp;&nbsp;
+                        <a href="" rel="nofollow">浙卫(03)网审[2014]015号</a>&nbsp;&nbsp;
+                        <a href="" rel="nofollow">(浙)-经营性-2014-0022</a>&nbsp;&nbsp;
+                        <a class="alliance" href="" target="_blank" rel="nofollow"></a>
+                        <a class="alliance attestation" href="" target="_blank" rel="nofollow"></a>
+                        <a class="record" href="" rel="nofollow" target="_blank"></a>
+
+                </p>
+                <div style="width:300px;margin:0 auto; padding:20px 0;">
+                    <a target="_blank" href="" rel="nofollow" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="/Public/home/images/ghs.png" style="float:left;" />
+                        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">苏ICP备16006825号-4</p>
+                    </a>
+                </div> -->
+            </div>
+        </div>
+
 
 		<div id="gm-bg"></div>
 <div id="gm-mask">
